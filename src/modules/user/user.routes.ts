f5023cdbdbf3d791ebe0e5e2d75b782b86a7a156
AminulIslamorange <aminulislamorange@gmail.com>
2026-09-24 17:32:58 +0600

    auth

--- src/modules/user/user.routes.ts
+++ src/modules/user/user.routes.ts
@@ -4,56 +4,115 @@ import { jwtUtils } from "../../utils/jwt";
 import config from "../../config";
 import { Role } from "../../../generated/prisma/enums";
 import httpStatus from 'http-status'
+import { catchAsync } from "../../utils/catchAsync";
+import { JwtPayload } from "jsonwebtoken";
+import { prisma } from "../../lib/prisma";
 
-const router=Router();
+const router = Router();
 
-declare global{
-  namespace Express {
-    interface Request{
-        user?:{
-            email:string;
-            name:string;
-            id:string;
-            role:Role;
+declare global {
+    namespace Express {
+        interface Request {
+            user?: {
+                email: string;
+                name: string;
+                id: string;
+                role: Role;
+            }
+
         }
-
     }
-  }  
 }
 
 
 
 router.post('/register', userController.registerUser);
-router.get('/me',(req:Request,res:Response,next:NextFunction
 
-)=>{
-     const {accessToken}=req.cookies;
-    const verifiedToken=jwtUtils.verifyToken(accessToken,config.jwt_access_secret);
-    
 
-    
 
-    if(typeof verifiedToken ==="string"){
-        throw new Error(verifiedToken)
-    }
-    const {email,name,id,role}=verifiedToken;
-    const requiredRoles=[Role.ADMIN,Role.AUTHOR,Role.USER]
-    if(! requiredRoles.includes(role)){
-        return res.status(403).json({
-            success:false,
-            statusCode:httpStatus.FORBIDDEN,
-            message:"Forbidded.YOu don't have permission to access"
+
+const auth = (...requiredRoles: Role[]) => {
+    return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+        const token = req.cookies.accessToken ||
+            (req.headers.authorization?.startsWith("Bearer") ? req.headers.authorization?.split(' ')[1]
+                : req.headers.authorization);
+
+        if (!token) {
+            throw new Error("You are not logged in.Please Login.")
+        }
+        const verifiedToken = jwtUtils.verifyToken(token, config.jwt_access_secret);
+
+
+
+        if (!verifiedToken.success) {
+            throw new Error(verifiedToken.error);
+        }
+        const { email, name, id, role } = verifiedToken.data as JwtPayload;
+
+        if (requiredRoles.length && !requiredRoles.includes(role)) {
+            throw new Error("Forbidded.YOu don't have permission to access")
+        }
+
+        const user=await prisma.user.findUnique({
+            where:{
+                id,email,name,role
+            }
         })
-    }
-    req.user={
-        email,
-        name,
-        id,
-        role
+        if(!user){
+            throw new Error ("User not found.Please log in again")
+        }
+        if(user.activeStatus=="BLOCKED"){
+      throw new Error("Your account has been blocked.Please contact support")
+        }
 
-    }
-    next();
-},userController.getMyProfile)
+        req.user={
+            email,name,id,role
+        }
+         next();
+    })
+   
+}
 
 
-export const userRoutes=router;
+
+
+
+
+router.get('/me',
+//      (req: Request, res: Response, next: NextFunction
+
+// ) => {
+//     const { accessToken } = req.cookies;
+//     const verifiedToken = jwtUtils.verifyToken(accessToken, config.jwt_access_secret);
+
+
+
+//     if (!verifiedToken.success) {
+//         throw new Error(verifiedToken.error);
+
+
+//     }
+
+//     const { email, name, id, role } = verifiedToken.data as JwtPayload;
+//     const requiredRoles = [Role.ADMIN, Role.AUTHOR, Role.USER]
+//     if (!requiredRoles.includes(role)) {
+//         return res.status(403).json({
+//             success: false,
+//             statusCode: httpStatus.FORBIDDEN,
+//             message: "Forbidded.YOu don't have permission to access"
+//         })
+//     }
+//     req.user = {
+//         email,
+//         name,
+//         id,
+//         role
+
+//     }
+//     next();
+// },
+
+ auth(Role.ADMIN,Role.USER,Role.USER), userController.getMyProfile)
+
+
+export const userRoutes = router;
